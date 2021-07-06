ce2f36d79badb942dd47b634cbaabe74b549f867
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-06 23:26:52 +0530

    Add files via upload

---
+++ jewels-and-stones.cpp
@@ -0,0 +1,47 @@
+//question-771//
+
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+ 
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+
+
+
+//solution//
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int n1 = jewels.size();
+        int n2 = stones.size();
+        int count=0;
+        for(int i=0;i<n1;i++)
+        {
+            for(int j=0;j<n2;j++)
+            {
+                if(jewels[i]==stones[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
