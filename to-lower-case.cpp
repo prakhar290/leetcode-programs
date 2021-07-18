2af0768ea85ef9b362c681748e34bc583cd1a8f1
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-19 00:56:57 +0530

    Add files via upload

---
+++ to-lower-case.cpp
@@ -0,0 +1,35 @@
+//question//
+
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+
+
+//solution//
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+       transform(s.begin(),s.end(),s.begin(), :: tolower);
+        return s;
+    }
+};
