d335c24ec022847575b3882798f565f7763d6d28
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-19 00:54:03 +0530

    Add files via upload

---
+++ sum-of-unique-elements.cpp
@@ -0,0 +1,62 @@
+//question//
+
+You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+Return the sum of all the unique elements of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,2]
+Output: 4
+Explanation: The unique elements are [1,3], and the sum is 4.
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: There are no unique elements, and the sum is 0.
+Example 3:
+
+Input: nums = [1,2,3,4,5]
+Output: 15
+Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+
+
+//solution//
+
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum=0;
+        sort(nums.begin(),nums.end());
+      for(int i=0;i<nums.size();)
+      {
+          int count=1;
+          for(int j=i+1;j<nums.size();j++)
+          {
+              if(nums[j]==nums[i])
+              {
+                  count++;
+              }
+          }
+          if(count>1)
+          {
+              i = i+count;
+              continue;
+          }
+          else
+          {
+              sum += nums[i];
+              i++;
+          }
+      }
+        return sum;
+    }
+};
