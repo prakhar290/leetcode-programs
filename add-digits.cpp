7d62f689a922aafcbc4f24e46f48e9e9a638040e
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-25 23:55:04 +0530

    Add files via upload

---
+++ add-digits.cpp
@@ -0,0 +1,46 @@
+//question//
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+
+
+//solution//
+
+
+class Solution {
+public:
+    int addDigits(int num) {
+       unsigned int ans = 0;
+        while(num>0)
+        {
+            unsigned int rem = num%10;
+            ans += rem;
+            num = num/10;
+            if(num==0 && ans>9)
+            {
+                num = ans;
+                ans = 0;
+            }
+        }
+        return ans;
+    }
+};
