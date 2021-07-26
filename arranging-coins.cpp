7bc0cbc210fa9a05a416eedaf0e89d13f76d1289
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-26 23:13:58 +0530

    Add files via upload

---
+++ arranging-coins.cpp
@@ -0,0 +1,50 @@
+//question//
+
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+ 
+
+Example 1:
+1
+1 1
+1 1 -
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+1
+1 1
+1 1 1
+1 1 - - 
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+
+
+
+//solution//
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(n>=i)
+            {
+                count++;
+                n -= i;
+            }
+        }
+        return count;
+    }
+};
