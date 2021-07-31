d62f03fbfbb54c012f503193b3e7cf3581fc11dc
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-07-31 16:11:06 +0530

    Add files via upload

---
+++ di-string-match.cpp
@@ -0,0 +1,58 @@
+//question//
+
+
+A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
+
+s[i] == 'I' if perm[i] < perm[i + 1], and
+s[i] == 'D' if perm[i] > perm[i + 1].
+Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.
+
+ 
+
+Example 1:
+
+Input: s = "IDID"
+Output: [0,4,1,3,2]
+Example 2:
+
+Input: s = "III"
+Output: [0,1,2,3]
+Example 3:
+
+Input: s = "DDI"
+Output: [3,2,0,1]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either 'I' or 'D'.
+
+
+
+//solution//
+
+
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> v;
+        int n = s.size();
+        int start = 0;
+        for(int i=0;s[i]!='\0';i++)
+        {
+            if(s[i]=='I')
+            {
+                v.push_back(start);
+                start++;
+            }
+            else
+            {
+                v.push_back(n);
+                n--;
+            }
+        }
+        v.push_back(start);
+        return v;
+    }
+};
