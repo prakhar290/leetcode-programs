e982568d48084a906570fd503bb2d69add54367b
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-08-27 22:41:30 +0530

    Add files via upload

---
+++ sql questions/second-highest-salary.sql
@@ -0,0 +1,28 @@
+//question//
+
+
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+
+
+
+//solution//
+
+
+# Write your MySQL query statement below
+select max(salary) as SecondHighestSalary from Employee where salary<(select max(salary) from Employee);
