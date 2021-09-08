92f6f57006778222f815ec44986ea8ffae866507
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-09-08 23:53:55 +0530

    Add files via upload

---
+++ middle-of-the-linked-list.cpp
@@ -0,0 +1,67 @@
+//question//
+
+
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+
+
+
+//solution//
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int findlength(ListNode* head)
+    {
+        int count=0;
+        while(head!=NULL)
+        {
+            count++;
+            head = head->next;
+        }
+        return count;
+    }
+    ListNode* middleNode(ListNode* head) {
+        int len = findlength(head);
+        int num;
+        num = len/2;
+        while(num--)
+        {
+            head = head->next;
+        }
+        return head;
+    }
+    
+};
