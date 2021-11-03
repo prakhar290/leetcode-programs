ccc14e74b2eaf460bc9f92ddbdc2d9f051ca9ac6
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-11-03 19:32:36 +0530

    Add files via upload

---
+++ sum-of-all-odd-length-subarrays.cpp
@@ -0,0 +1,70 @@
+//question//
+
+
+Given an array of positive integers arr, calculate the sum of all possible odd-length subarrays.
+
+A subarray is a contiguous subsequence of the array.
+
+Return the sum of all odd-length subarrays of arr.
+
+ 
+
+Example 1:
+
+Input: arr = [1,4,2,5,3]
+Output: 58
+Explanation: The odd-length subarrays of arr and their sums are:
+[1] = 1
+[4] = 4
+[2] = 2
+[5] = 5
+[3] = 3
+[1,4,2] = 7
+[4,2,5] = 11
+[2,5,3] = 10
+[1,4,2,5,3] = 15
+If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+Example 2:
+
+Input: arr = [1,2]
+Output: 3
+Explanation: There are only 2 subarrays of odd length, [1] and [2]. Their sum is 3.
+Example 3:
+
+Input: arr = [10,11,12]
+Output: 66
+ 
+
+Constraints:
+
+1 <= arr.length <= 100
+1 <= arr[i] <= 1000
+
+
+//solution//
+
+
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+     int finalsum = 0;
+        for(int i=0;i<arr.size();i++)
+        {
+            int sum = 0;
+            vector<int> v;
+            v.push_back(arr[i]);
+            sum += arr[i];
+            finalsum += sum;
+            for(int j=i+1;j<arr.size();j++)
+            {
+                sum += arr[j];
+                v.push_back(arr[j]);
+                if(v.size()%2!=0)
+                {
+                    finalsum += sum;
+                }
+            }
+        }
+        return finalsum;
+    }
+};
