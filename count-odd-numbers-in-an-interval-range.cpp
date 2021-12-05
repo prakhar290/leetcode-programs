ff6bbeeb6461a67dbaa3e80800e4322c315d90a2
Prakhar Varshney <68542024+prakhar290@users.noreply.github.com>
2021-12-05 22:34:09 +0530

    Update count-odd-numbers-in-an-interval-range.cpp

--- count-odd-numbers-in-an-interval-range.cpp
+++ count-odd-numbers-in-an-interval-range.cpp
@@ -38,3 +38,19 @@ public:
         return count;
     }
 };
+
+//another solution//
+
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int dif = high-low;
+        if(high%2!=0 || low%2!=0)
+        {
+            return dif/2+1;
+        }
+        else
+            return dif/2;
+    }
+};
